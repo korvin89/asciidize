5297ba5c26da28046fb9d8f8fd91117de0bb8481
Alaev Evgeny <alaev89@yandex.ru>
2024-09-24 23:55:45 +0200

    fix: fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-// use asciidize::image_converter::image_converter::bitmap_to_grayscale;
-// use asciidize::utils::bitmap::ImageType;
-
 fn main() {
-    // println!("Hello, world!");
-    // bitmap_to_grayscale("/Users/alaev89/Desktop/pets/asciidize/src/test.png", &ImageType::PNG)
+    println!("Hello, world!");
 }
 
 #[cfg(test)]
