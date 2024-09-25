6f2f956e7507739c6a1f645190a56e9b7fccbe98
ovsds <ovsds@yandex-team.ru>
2024-09-25 23:48:41 +0200

    test: add assert_fs to file tests

--- src/utils/test.rs
+++ src/utils/test.rs
@@ -1,34 +1,18 @@
-use super::bitmap;
-
 const EXPECTED_IMAGE_FOLDER_PATH: &str = "tests/test_images";
-const TEST_IMAGE_FOLDER_PATH: &str = "tests//tmp";
 
 #[must_use]
-pub fn get_expected_file_path(
-    image_type: &bitmap::ImageType,
-    image_name: &str,
-) -> std::string::String {
+pub fn get_expected_file_path(image_name: &str) -> std::string::String {
     let folder_path = std::path::Path::new(EXPECTED_IMAGE_FOLDER_PATH);
-    let file_path = folder_path
-        .join(image_name)
-        .with_extension(match image_type {
-            bitmap::ImageType::PNG => "png",
-        });
+    let file_path = folder_path.join(image_name);
     return file_path.to_string_lossy().to_string();
 }
 
-/// # Panics
-///
-/// Will panic if could not create folder by `folder_path`
 #[must_use]
-pub fn get_test_file_path(image_type: &bitmap::ImageType, image_name: &str) -> std::string::String {
-    let folder_path = std::path::Path::new(TEST_IMAGE_FOLDER_PATH);
-    std::fs::create_dir_all(folder_path).unwrap();
+pub fn get_test_file_path(image_name: &str) -> std::string::String {
+    let folder_path = assert_fs::fixture::TempDir::new()
+        .unwrap()
+        .into_persistent();
 
-    let file_path = folder_path
-        .join(image_name)
-        .with_extension(match image_type {
-            bitmap::ImageType::PNG => "png",
-        });
+    let file_path = folder_path.join(image_name);
     return file_path.to_string_lossy().to_string();
 }
