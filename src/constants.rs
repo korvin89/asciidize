6a27d5955e02b9b64803a2bc652a724e823593ce
ovsds <ovsds@yandex-team.ru>
2024-10-13 20:34:14 +0200

    feat: add border to sample print

--- src/constants.rs
+++ src/constants.rs
@@ -6,7 +6,9 @@ ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`\
 abcdefghijklmnopqrstuvwxyz{|}~"; // ASCII symbols from 32 to 126
 
 // AlphabetSampleString
-pub const ALPHABET_SAMPLE_WIDTH: usize = 20;
+pub const ALPHABET_SAMPLE_WIDTH: usize = 10; // sqrt(ALPHABET_SYMBOL_STR.len())
 pub const ALPHABET_SAMPLE_FILLER_SYMBOL: char = ' ';
+pub const ALPHABET_SAMPLE_BORDER_SYMBOL: char = 'X';
+pub const ALPHABET_SAMPLE_BORDER_WIDTH: usize = 1;
 pub const ALPHABET_SAMPLE_PADDING_SYMBOL: char = ' ';
 pub const ALPHABET_SAMPLE_PADDING_WIDTH: usize = 1;
--- tests/cli/alphabet/print.rs
+++ tests/cli/alphabet/print.rs
@@ -9,7 +9,7 @@ fn test_print() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("--width").arg("3");
     cmd.arg("--alphabet").arg("abcdefg");
 
-    let expected_stdout = "     \n abc \n def \n g   \n     \n";
+    let expected_stdout = "       \n XXXXX \n XabcX \n XdefX \n Xg  X \n XXXXX \n       \n";
 
     cmd.assert().success();
     cmd.assert().code(0);
