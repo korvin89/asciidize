74acde93f406369fb07886b637dc28caad2471d0
ovsds <ovsds@yandex-team.ru>
2024-10-14 21:56:36 +0200

    feat: add width, height to sample

--- src/alphabet/sample.rs
+++ src/alphabet/sample.rs
@@ -1,15 +1,13 @@
 use crate::alphabet::Alphabet;
 
+#[derive(Debug, PartialEq)]
 pub struct Sample {
-    chars: Vec<Vec<char>>,
+    pub chars: Vec<Vec<char>>,
+    pub width: usize,
+    pub height: usize,
 }
 
 impl Sample {
-    #[must_use]
-    fn new(chars: Vec<Vec<char>>) -> Sample {
-        return Sample { chars };
-    }
-
     #[must_use]
     pub fn from_alphabet(alphabet: &Alphabet, width: usize, filler_symbol: char) -> Sample {
         let mut chars = vec![];
@@ -25,14 +23,20 @@ impl Sample {
             chars.push(row);
         }
 
-        return Sample::new(chars);
+        let height = chars.len();
+
+        return Sample {
+            chars,
+            width,
+            height,
+        };
     }
 
     pub fn add_padding(&mut self, padding_symbol: char, padding_width: usize) {
         let mut chars = vec![];
 
-        let original_width = self.chars[0].len();
-        let result_width = original_width + 2 * padding_width;
+        let result_width = self.width + 2 * padding_width;
+        let result_height = self.height + 2 * padding_width;
 
         for _ in 0..padding_width {
             let mut row = vec![];
@@ -65,6 +69,8 @@ impl Sample {
         }
 
         self.chars = chars;
+        self.width = result_width;
+        self.height = result_height;
     }
 }
 
@@ -89,39 +95,49 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let sample = Sample::new(vec![vec!['A', 'B'], vec!['C', 'D']]);
-        assert_eq!(sample.chars, vec![vec!['A', 'B'], vec!['C', 'D']]);
-    }
-
-    #[test]
     fn test_from_alphabet() {
         let alphabet = Alphabet::new(vec!['A', 'B', 'C', 'D', 'E']);
         let sample = Sample::from_alphabet(&alphabet, 2, ' ');
         assert_eq!(
-            sample.chars,
-            vec![vec!['A', 'B'], vec!['C', 'D'], vec!['E', ' ']]
+            sample,
+            Sample {
+                chars: vec![vec!['A', 'B'], vec!['C', 'D'], vec!['E', ' ']],
+                width: 2,
+                height: 3,
+            }
         );
     }
 
     #[test]
     fn test_add_padding() {
-        let mut sample = Sample::new(vec![vec!['A', 'B'], vec!['C', 'D']]);
+        let mut sample = Sample {
+            chars: vec![vec!['A', 'B'], vec!['C', 'D']],
+            width: 2,
+            height: 2,
+        };
         sample.add_padding(' ', 1);
         assert_eq!(
-            sample.chars,
-            vec![
-                vec![' ', ' ', ' ', ' '],
-                vec![' ', 'A', 'B', ' '],
-                vec![' ', 'C', 'D', ' '],
-                vec![' ', ' ', ' ', ' ']
-            ]
+            sample,
+            Sample {
+                chars: vec![
+                    vec![' ', ' ', ' ', ' '],
+                    vec![' ', 'A', 'B', ' '],
+                    vec![' ', 'C', 'D', ' '],
+                    vec![' ', ' ', ' ', ' '],
+                ],
+                width: 4,
+                height: 4,
+            }
         );
     }
 
     #[test]
     fn test_display() {
-        let sample = Sample::new(vec![vec!['A', 'B'], vec!['C', 'D']]);
+        let sample = Sample {
+            chars: vec![vec!['A', 'B'], vec!['C', 'D']],
+            width: 2,
+            height: 2,
+        };
         let result = sample.to_string();
         assert_eq!(result, "AB\nCD");
     }
